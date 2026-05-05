45bbbbf84b536c7a66639b888cebc5c5e95ee0d4
zu2220 <zu22206853@utp.edu.pe>
2026-05-04 22:30:21 -0500

    i've create auth context

---
+++ src/main/java/com/clinica_javierprado/cjp_backend/AuthenticationContext/domain/User.java
@@ -0,0 +1,11 @@
+package com.clinica_javierprado.cjp_backend.AuthenticationContext.domain;
+
+public class User {
+
+    private UUID uuid;
+    private Names names;
+    private int dni;
+    private 
+    
+
+}
---
+++ src/main/java/com/clinica_javierprado/cjp_backend/AuthenticationContext/domain/valueObjects/UserLastNames.java
@@ -0,0 +1,5 @@
+package com.clinica_javierprado.cjp_backend.AuthenticationContext.domain.valueObjects;
+
+public class UserLastNames {
+
+}
