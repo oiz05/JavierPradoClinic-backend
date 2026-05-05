6c4a0fd6fd815fcc6329e1e0b64c0d088b2949f0
oscarz <isaac.zari60@gmail.com>
2026-05-04 21:39:08 -0500

    initial changes

---
+++ src/main/java/com/clinica_javierprado/cjp_backend/CjpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clinica_javierprado.cjp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinica_javierprado/cjp_backend/CjpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica_javierprado.cjp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
